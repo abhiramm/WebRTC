b33cb502de3c617fabd9e8d7ded43e30cc274b8e
Abhirama sundram <abhiramasundram2007@gmail.com>
2014-03-05 15:51:40 +0530

    initial push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WebRTC::Application.config.secret_key_base = 'a500e6e805a6de91633b570168824790387ebcc511b1ed8cd835a3f551ae2c97b8ff0615a536b2b7fe34019e3489d2a7cc716e5135f8bd4e456559209ce3cea5'
