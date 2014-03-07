31c64035b439fec2eab12a52045eec8256ac1229
Aravind <pro.aravind@gmail.com>
2014-03-07 18:33:38 +0530

    new changes to opentok implementation

--- app/assets/javascripts/opentok_client.js
+++ app/assets/javascripts/opentok_client.js
@@ -6,7 +6,14 @@ $('document').ready(function(){
 	var token     = "T1==cGFydG5lcl9pZD00NDY4MDkyMiZzZGtfdmVyc2lvbj10YnJ1YnktdGJyYi12MC45MS4yMDExLTAyLTE3JnNpZz1jNGE4YWNhMzI1MmMyMDdlMzE1OWFmZTBlZjFhYzBlNmExY2I4MjZlOnJvbGU9cHVibGlzaGVyJnNlc3Npb25faWQ9Ml9NWDQwTkRZNE1Ea3lNbjUtUm5KcElFMWhjaUF3TnlBd05Eb3pOem8wTnlCUVUxUWdNakF4Tkg0d0xqQXdOalV6TnpJMU9EWi0mY3JlYXRlX3RpbWU9MTM5NDE5NTg5NSZub25jZT0wLjM0MTk0ODU4MjM0NzE4OTk0JmV4cGlyZV90aW1lPTEzOTY3ODc4NjAmY29ubmVjdGlvbl9kYXRhPQ==";
 
 	function sessionConnectedHandler (event) {
-		session.publish( publisher );
+		session.publish( publisher,function(){
+		$('#countdown').timeTo({
+		seconds: 100,
+		countdown: true
+	},function(){ 
+		opentok_client_hangup();
+	});	
+		});
 		subscribeToStreams(event.streams);
 	}
 	function subscribeToStreams(streams) {
@@ -31,3 +38,8 @@ $('document').ready(function(){
 	session.addEventListener("streamCreated",	streamCreatedHandler);
 	}
 	});
+function opentok_client_hangup
+{
+	session.disconnect();
+	$("#countdown").timeTo("stop");
+}
